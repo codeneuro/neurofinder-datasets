53e7a7ac014f016377a54a30d29db3cbfed2f09e
freeman <the.freeman.lab@gmail.com>
2016-03-22 23:19:44 -0400

    include conversion to tif

--- zip-testing.py
+++ zip-testing.py
@@ -2,6 +2,9 @@
 
 import sys
 import os
+from numpy import frombuffer
+from glob import glob
+import skimage.external.tifffile as tifffile
 
 name = sys.argv[1]
 
@@ -16,6 +19,18 @@ os.system('mv %s neurofinder.%s' % (name, name))
 
 print('removing sources\n\n')
 os.system('rm -rf neurofinder.%s/sources' % name)
+
+print('converting images to tif\n\n')
+with open('neurofinder.%s/images/conf.json' % name) as f:
+    dims = json.load(f)['dims']
+files = glob('neurofinder.%s/images/*/*.bin' % name)
+def toarray(f):
+    with open(f) as fid:
+        return frombuffer(fid.read(),'uint16').reshape(dims, order='F')
+for i, f in enumerate(files):
+    tifffile.imsave(toarray(f), 'neurofinder.%s/images-tif/image%04g.tiff' % (name, i))
+os.system('rm -rf neurofinder.%s/images' % name)
+os.system('mv neurofinder.%s/images-tif neurofinder.%s/images' % name)
 
 print('creating zip\n\n')
 os.system('zip -r neurofinder.%s.zip neurofinder.%s' % (name, name))
--- zip-training.py
+++ zip-training.py
@@ -2,6 +2,9 @@
 
 import sys
 import os
+from numpy import frombuffer
+from glob import glob
+import skimage.external.tifffile as tifffile
 
 name = sys.argv[1]
 
@@ -13,6 +16,21 @@ os.system('cp neurofinder/datasets/README.md %s/' % name)
 os.system('cp neurofinder/datasets/example.py %s/' % name)
 os.system('cp neurofinder/datasets/example.m %s/' % name)
 os.system('mv %s neurofinder.%s' % (name, name))
+
+print('renaming sources\n\n')
+os.system('mv neurofinder.%s/sources/sources.json neurofinder.%s/regions/regions.json' % (name, name))
+
+print('converting images to tif\n\n')
+with open('neurofinder.%s/images/conf.json' % name) as f:
+    dims = json.load(f)['dims']
+files = glob('neurofinder.%s/images/*/*.bin' % name)
+def toarray(f):
+    with open(f) as fid:
+        return frombuffer(fid.read(),'uint16').reshape(dims, order='F')
+for i, f in enumerate(files):
+    tifffile.imsave(toarray(f), 'neurofinder.%s/images-tif/image%04g.tiff' % (name, i))
+os.system('rm -rf neurofinder.%s/images' % name)
+os.system('mv neurofinder.%s/images-tif neurofinder.%s/images' % name)
 
 print('creating zip\n\n')
 os.system('zip -r neurofinder.%s.zip neurofinder.%s' % (name, name))
