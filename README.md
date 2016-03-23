6febdd0642c87f52bc603c03feda430a50cb4618
freeman <the.freeman.lab@gmail.com>
2016-03-23 09:50:12 -0400

    add node.js example

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This README describes the datasets for the [neurofinder](http://neurofinder.code
 
 Training datasets are provided with ground truth labeled regions for identified neurons, and testing datasets are provided without ground truth. Each downloadable dataset includes metadata (as `.json`), images (as `.tiff`), and coordinates of identified neurons, also known as ROIs (as `.json`). Datasets are around 1 GB zipped and a few GBs unzipped. Visit the [neurofinder](https://github.com/codeneuro/neurofinder) repository for current download links for all datasets.
 
-Along with the data itself, each download includes example loading scripts in `python` and `MATLAB`, the source code of which are in this [reposistory](https://github.com/codeneuro/neurofinder-datasets). 
+Along with the data itself, each download includes example loading scripts in `python`, `MATLAB`, and `node.js`, the source code of which are in this [reposistory](https://github.com/codeneuro/neurofinder-datasets). 
 
 To contribute example loading scripts for other languages, just submit a pull request! If there are problems with the loading scripts, submit an issue on GitHub.
---
+++ example.js
@@ -0,0 +1,51 @@
+// example node.js script for loading neurofinder data
+// 
+// for more info see:
+//
+// - http://neurofinder.codeneuro.org
+// - https://github.com/codeneuro/neurofinder
+//
+// requires a few node.js modules, install with `npm install module`
+//
+// "ndarray": "^1.0.18",
+// "ndarray-imshow": "^1.0.1",
+// "ndarray-ops": "^1.2.2",
+// "progress": "^1.1.8",
+// "tiff": "^1.1.0",
+// "zeros": "^1.0.0"
+//
+
+var fs = require('fs')
+var tiff = require('tiff')
+var progress = require('progress')
+var zeros = require('zeros')
+var ndarray = require('ndarray')
+var ops = require('ndarray-ops')
+var imshow = require('ndarray-imshow')
+
+// load images
+var sum, mat, image, buffer
+var files = fs.readdirSync('images')
+var bar = new progress('loading [:bar] :percent :etas', { total: files.length , width: 50})
+
+files.forEach(function (file, index) {
+  buffer = fs.readFileSync('images/' + file)
+  image = new tiff.TIFFDecoder(buffer).decode().ifd[0]
+  mat = ndarray(image.data, [image.width, image.height])
+  if (!sum) sum = zeros([image.width, image.height])
+  ops.add(sum, mat, sum)
+  bar.tick()
+})
+
+// load regions
+var blob = JSON.parse(fs.readFileSync('regions/regions.json'))
+var regions = blob.map(function (region) {return region.coordinates})
+var mask = zeros(sum.shape)
+regions.forEach(function (region) {
+  region.forEach(function (coordinate) {
+    mask.set(coordinate[0], coordinate[1], 1)
+  })
+})
+
+imshow(mask, {colormap: 'gray'})
+imshow(sum, {colormap: 'gray'})
